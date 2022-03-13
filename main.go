3bf9275227089db1739a0a8dd9fa77b4a8ed7874
B L Λ C W <37225059+blackcrw@users.noreply.github.com>
2022-03-13 15:38:33 -0300

    😒 improvements in returning information to the user

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
+/* Lista de Tarefas:
+
+1. Criar um signal para SHIFT+C
+2. Testar as novas funções
+
+*/
+
 package main
 
 import (
-	"os"
 	"sync"
 	"time"
 
+	"github.com/blackcrw/apkron/internal"
 	"github.com/blackcrw/apkron/internal/apktool"
 	"github.com/blackcrw/apkron/internal/printer"
 	temp_path "github.com/blackcrw/apkron/internal/temp"
@@ -13,38 +20,43 @@ import (
 
 var wg sync.WaitGroup
 
-func check_error(err error) {
-	if err != nil {
-		printer.Printf("%s[X]%s %s\n", printer.RED, printer.RESET, err)
-		
-		defer os.Exit(0)
-	}
+func init() {
+	internal.InitApkron()
 }
 
-func main() {
-	check_error(apktool.Exists())
-	printer.Printf("%s[+]%s %s\n", printer.GREEN, printer.RESET, "Apk Tool Exists\n")
-	
-	var temporary_directory, err = temp_path.CreateTemporaryDirectory()
-	defer temp_path.DeleteTemporaryDirectory(temporary_directory)
+func main() {	
+	var temporary_directory, err = temp_path.CreateTempDirectory()
+	defer temp_path.DeleteTempDirectory(temporary_directory)
 
-	check_error(err)
-	printer.Printf("%s[+]%s %s: %s\n", printer.GREEN, printer.RESET, "Temporary Directory Created", temporary_directory)
+	{	printer.CError(err)
+		printer.Info("Temporary Directory Created", temporary_directory)	}
 
 	wg.Add(1)
 
 	go func() {
-		var _, err = apktool.Decompile("decompile_test.apk", temporary_directory)
-		check_error(err)
+		{	var _, err = apktool.Decompile("decompile_test.apk", temporary_directory)
+			printer.CError(err)	}
 		
 		defer wg.Done()
 	}()
 
-	printer.Printf("%s[+]%s %s\n", printer.GREEN, printer.RESET, "Decompiling application!")
+	printer.Done("Decompiling application!\n")
 	
 	wg.Wait()
 	
-	interesting.AndroidManifest(temporary_directory)
+	printer.Info("Application Permissions:\n")
+	
+	if perms, err := interesting.AndroidManifest(temporary_directory); err != nil {
+		printer.CError(err)
+	} else if len(perms.Permissions) > 0 {
+		for _, x := range perms.Permissions {
+			printer.Done("Name:", x.Name)
+			printer.Printf("%s   -%s Description: %s\n", printer.MAGENTA, printer.RESET, x.Description)
+			printer.Printf("%s   -%s Match: %s\n\n", printer.MAGENTA, printer.RESET, x.Match)
+		}
+	} else {
+		printer.Danger("No permissions found")
+	}
 
 	time.Sleep(5*time.Second)
 }
